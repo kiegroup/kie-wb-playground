04af253b73a9afd1d7d2db9a7da03624b40e56a8
Ivo Bek <ibek@redhat.com>
2018-04-04 11:48:50 +0200

    New getting started Loan app project

---
+++ getstarted-loan/src/main/java/org/jboss/example/.ValidationErrorDO.java
@@ -0,0 +1,13 @@
+<org.uberfire.java.nio.base.Properties serialization="custom">
+  <unserializable-parents/>
+  <map>
+    <default>
+      <loadFactor>0.75</loadFactor>
+      <threshold>12</threshold>
+    </default>
+    <int>16</int>
+    <int>1</int>
+    <string>generated.generated</string>
+    <boolean>false</boolean>
+  </map>
+</org.uberfire.java.nio.base.Properties>
