b5794aa7a7b7d59869f606b9de1786403d6c0cbb
Edson Tirelli <ed.tirelli@gmail.com>
2019-02-08 16:47:21 -0500

    Adding DMN traffic violation example

---
+++ traffic-violation/src/test/java/org/kie/examples/ScenarioJunitActivatorTest.java
@@ -0,0 +1,7 @@
+package org.kie.examples;
+/**
+* Do not remove this file
+*/
+@org.junit.runner.RunWith(org.drools.workbench.screens.scenariosimulation.backend.server.runner.ScenarioJunitActivator.class)
+public class ScenarioJunitActivatorTest {
+}
