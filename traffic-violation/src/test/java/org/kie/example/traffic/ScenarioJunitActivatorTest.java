cd3b90a1d30ba9ee82d9883735c02bcd3838ed72
Daniele Zonca <daniele.zonca@gmail.com>
2019-05-29 00:27:38 +0200

    Fix pom and activator

--- traffic-violation/src/test/java/org/kie/example/traffic/ScenarioJunitActivatorTest.java
+++
@@ -1,7 +0,0 @@
-package org.kie.example.traffic;
-/**
-* Do not remove this file
-*/
-@org.junit.runner.RunWith(org.drools.workbench.screens.scenariosimulation.backend.server.runner.ScenarioJunitActivator.class)
-public class ScenarioJunitActivatorTest {
-}
